29b5f2d4fc25c14b0a0e721711c165832e917b1d
Arnab Saha <arnabsahawrk@gmail.com>
2026-03-01 16:21:27 +0600

    fix: google authentication callback url issue

--- backend/apps/accounts/urls.py
+++ backend/apps/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from apps.accounts.views import GoogleLoginView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+app_name = "accounts"
 urlpatterns = [
     path("google/", GoogleLoginView.as_view(), name="google_login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,9 +1,10 @@
 from dj_rest_auth.registration.views import SocialLoginView
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from decouple import config
 
 
 class GoogleLoginView(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:5173"
+    callback_url = config("GOOGLE_CALLBACK_URL")
     client_class = OAuth2Client
